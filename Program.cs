f54fec4f94b1a3372c465a3441709df252a2beb9
Edevaldo Cruz <edevaldo_cruz@hotmail.com>
2022-11-05 11:19:38 -0300

    Conclusao do desafio

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@
 var nokia = new Nokia("32 98888-8888", "Nokia C01 Plus", "123456789", 600);
 var iphone = new Iphone("32 97777-7777", "iPhone 14 Pro Max", "789456123", 900);
 
-
-// var nubank = new App("Nubank", "Nu", 400);
-// var tikTok = new App("TikTok", "TikTok Pte. Ltd.", 200);
-
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Uber", "Uber Technologies, Inc", 300);
